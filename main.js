ec94a0b375e77a9c1f7919771ca5ade465455cc7
abdallahhesham99 <abdallahhesham414@gmail.com>
2022-03-29 09:11:52 +0200

    push project

---
+++ main.js
@@ -0,0 +1,61 @@
+let titleInput = document.getElementById("titleInput");
+let priceInput = document.getElementById("priceInput");
+let taxesInput = document.getElementById("taxesInput");
+let adsInput = document.getElementById("adsInput");
+let discountInput = document.getElementById("discountInput");
+let totalSpan = document.getElementById("totalSpan");
+let countInput = document.getElementById("countInput");
+let categoryInput = document.getElementById("categoryInput");
+let searchInput = document.getElementById("searchInput");
+let submitButton = document.getElementById("submitButton");
+let deleteAllDiv = document.getElementById("deleteAllDiv");
+// "#28A745"
+// "#6C757D"
+let mood = "create";
+let searchMood = "title";
+
+let dummy;
+
+// Function => getTotal Price of Product
+
+// *=============================================================
+
+
+// Function => Create Product
+submitButton.addEventListener("click", function () {});
+// *=============================================================
+
+//* =============================================================
+// Function => Clear Inputs
+function clearData() {
+  titleInput.value = "";
+  priceInput.value = "";
+  taxesInput.value = "";
+  adsInput.value = "";
+  discountInput.value = "";
+  totalSpan.innerHTML = "";
+  countInput.value = "";
+  categoryInput.value = "";
+}
+
+//* =============================================================
+// Function => Read Products
+
+// *=============================================================
+// Function => Delete Product
+
+
+// Function => Delete All Products
+
+//* =============================================================
+
+// *=============================================================
+// Function => Update Product
+
+// *=============================================================
+// Function => Search Mood
+
+// Function ===> Search Product
+
+// *=============================================================
+//validation
