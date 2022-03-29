90657a464e38135a63873ec3e262db57ba0987ae
abdallahhesham99 <abdallahhesham414@gmail.com>
2022-03-29 09:40:29 +0200

    create and clear function

--- main.js
+++ main.js
@@ -6,23 +6,62 @@ let discountInput = document.getElementById("discountInput");
 let totalSpan = document.getElementById("totalSpan");
 let countInput = document.getElementById("countInput");
 let categoryInput = document.getElementById("categoryInput");
+//
 let searchInput = document.getElementById("searchInput");
+//
 let submitButton = document.getElementById("submitButton");
+let clearButton = document.getElementById("clearButton");
 let deleteAllDiv = document.getElementById("deleteAllDiv");
-// "#28A745"
-// "#6C757D"
-let mood = "create";
-let searchMood = "title";
-
-let dummy;
 
 // Function => getTotal Price of Product
+function getTotal() {
+  if (priceInput.value != "") {
+    let totalResult =
+      +priceInput.value +
+      +taxesInput.value +
+      +adsInput.value -
+      +discountInput.value;
 
+    totalSpan.innerHTML = totalResult;
+    totalSpan.style.backgroundColor = "#28A745";
+  } else {
+    totalSpan.innerHTML = ``;
+    totalSpan.style.backgroundColor = "#6C757D";
+  }
+}
 // *=============================================================
 
+//empty variable for contain all products data
+let productsData;
 
+//if there data in localstorage named productsdata set it in productData variable
+//else make empty array
+if (localStorage.getItem("productsData")) {
+  productsData = JSON.parse(localStorage.getItem("productsData"));
+} else {
+  productsData = [];
+}
 // Function => Create Product
-submitButton.addEventListener("click", function () {});
+function createProduct() {
+  //make product object
+  let productObj = {
+    title: titleInput.value.toLowerCase(),
+    price: priceInput.value,
+    taxes: taxesInput.value,
+    ads: adsInput.value,
+    discount: discountInput.value,
+    total: totalSpan.innerHTML,
+    count: countInput.value,
+    category: categoryInput.value.toLowerCase(),
+  };
+  productsData.push(productObj);
+
+  localStorage.setItem("productsData", JSON.stringify(productsData));
+
+  console.log(productsData);
+  clearData();
+}
+submitButton.addEventListener("click", createProduct);
 // *=============================================================
 
 //* =============================================================
@@ -37,13 +76,13 @@ function clearData() {
   countInput.value = "";
   categoryInput.value = "";
 }
+clearButton.addEventListener("click", clearData);
 
 //* =============================================================
 // Function => Read Products
 
 // *=============================================================
 // Function => Delete Product
-
 
 // Function => Delete All Products
 
